162fe5f2f9a53325af4bb6d9e1fa26e1534a7314
foresterre <foresterre@users.noreply.github.com>
2026-08-03 15:46:45 +0000

    deploy: 2ade118519f95693bef4d4933e2d16226132d9de

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-75c23ebd.js',
+                path_to_root + 'searchindex-0416ff26.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
